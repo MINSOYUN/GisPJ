58c828f60552dd159af4864beea9747b847a1690
소윤 <syhymin@gmail.com>
2023-09-14 12:02:12 +0900

    노트북(1536px)에 맞춘 반응형 웹 구현 sidebar width 값 25%로 증가

--- GisPJ/src/main/webapp/WEB-INF/views/gis/vworld.jsp
+++ GisPJ/src/main/webapp/WEB-INF/views/gis/vworld.jsp
@@ -27,9 +27,7 @@
           
         });
     </script>
-    <style type="text/css">
-    	
-    </style>
+   
     <!-- openlayers -->
     <link rel="stylesheet" href="https://openlayers.org/en/v4.6.5/css/ol.css" type="text/css">
     <script src="https://openlayers.org/en/v4.6.5/build/ol.js"></script>
--- GisPJ/src/main/webapp/resources/js/vworld.js
+++ GisPJ/src/main/webapp/resources/js/vworld.js
@@ -7,9 +7,46 @@ $(document).ready(function() {
     var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
     console.log("현재 창의 너비: " + windowWidth + "px");
-
     
 });
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const fileDropArea = document.getElementById("fileDropArea");
+
+  	// 드래그 앤 드롭 이벤트 처리
+  	fileDropArea.addEventListener("dragover", (e) => {
+  	   e.preventDefault();
+  	   fileDropArea.classList.add("hover");
+  	});
+  	
+  	fileDropArea.addEventListener("dragleave", () => {
+  	   fileDropArea.classList.remove("hover");
+  	});
+  	
+  	fileDropArea.addEventListener("drop", (e) => {
+  	   e.preventDefault();
+  	   fileDropArea.classList.remove("hover");
+  	
+  	   const files = e.dataTransfer.files;
+  	   handleFiles(files);
+  	});
+
+  //파일 업로드 처리 함수
+  function handleFiles(files) {
+  	for (const file of files) {
+  	   if (file.type === "text/csv") {
+  	       // CSV 파일을 업로드하는 로직 추가
+  	       console.log("업로드한 파일 이름: " + file.name);
+  	   } else {
+  	       alert("CSV 파일만 업로드할 수 있습니다.");
+  	   }
+  	}
+  }
+  
+});
+
 
 function init() {
 
@@ -398,39 +435,3 @@ $(function(){
     
 });
 
-
-
-const fileDropArea = document.getElementById("fileDropArea");
-
-//fileDropArea가 null이 아닌 경우에만 이벤트 리스너를 등록
-if (fileDropArea) {
-	// 드래그 앤 드롭 이벤트 처리
-	fileDropArea.addEventListener("dragover", (e) => {
-	   e.preventDefault();
-	   fileDropArea.classList.add("hover");
-	});
-	
-	fileDropArea.addEventListener("dragleave", () => {
-	   fileDropArea.classList.remove("hover");
-	});
-	
-	fileDropArea.addEventListener("drop", (e) => {
-	   e.preventDefault();
-	   fileDropArea.classList.remove("hover");
-	
-	   const files = e.dataTransfer.files;
-	   handleFiles(files);
-	});
-}
-
-//파일 업로드 처리 함수
-function handleFiles(files) {
-	for (const file of files) {
-	   if (file.type === "text/csv") {
-	       // CSV 파일을 업로드하는 로직 추가
-	       console.log("업로드한 파일 이름: " + file.name);
-	   } else {
-	       alert("CSV 파일만 업로드할 수 있습니다.");
-	   }
-	}
-}
